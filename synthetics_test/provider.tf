bde988f40fd6c7d8754ea743f8f9d093a88df6d5
Mohit Dubey <52689488+MohitDubeyGitHub@users.noreply.github.com>
2021-07-07 16:43:18 +0530

    Update provider.tf

--- synthetics_test/provider.tf
+++ synthetics_test/provider.tf
@@ -6,3 +6,11 @@ provider "datadog" {
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "iac-terra-backend"
+    key    = ""
+    region = "us-east-1"
+   }
+}
