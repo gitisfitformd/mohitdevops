ced92ce52f86e49cd6d86b2d30fe04b1cc80993a
Mohit Dubey <52689488+MohitDubeyGitHub@users.noreply.github.com>
2021-07-07 16:52:21 +0530

    Create variables.tf

---
+++ synthetics_test/variables.tf
@@ -0,0 +1,11 @@
+variable datadog_api_key {
+  type        = string
+  default     = ""
+  description = "API key for Datadog"
+}
+
+variable datadog_app_key {
+  type        = string
+  default     = ""
+  description = "APP key for Datadog"
+}
