ce1f10c6a0142b7ac88d3355c0bb90c04ac1944b
modubey <modubey@deloitte.com>
2021-07-07 17:54:10 +0530

    consolidated

--- resources/datadog_resources/outputs.tf
+++ resources/datadog_resources/outputs.tf
@@ -97,3 +97,27 @@ output "datadog_synthetics_test_tfer--synthetics_ycw-002D-cxr-002D-9f2_id" {
 output "datadog_synthetics_test_tfer--synthetics_yri-002D-6zy-002D-3r9_id" {
   value = "${datadog_synthetics_test.tfer--synthetics_yri-002D-6zy-002D-3r9.id}"
 }
+
+output "datadog_dashboard_tfer--dashboard_37x-002D-rv7-002D-rue_id" {
+  value = "${datadog_dashboard.tfer--dashboard_37x-002D-rv7-002D-rue.id}"
+}
+
+output "datadog_dashboard_tfer--dashboard_fx3-002D-z6r-002D-k69_id" {
+  value = "${datadog_dashboard.tfer--dashboard_fx3-002D-z6r-002D-k69.id}"
+}
+
+output "datadog_dashboard_tfer--dashboard_i3b-002D-2da-002D-ftr_id" {
+  value = "${datadog_dashboard.tfer--dashboard_i3b-002D-2da-002D-ftr.id}"
+}
+
+output "datadog_dashboard_tfer--dashboard_mzy-002D-zd3-002D-q4d_id" {
+  value = "${datadog_dashboard.tfer--dashboard_mzy-002D-zd3-002D-q4d.id}"
+}
+
+output "datadog_dashboard_tfer--dashboard_s6b-002D-ixt-002D-zke_id" {
+  value = "${datadog_dashboard.tfer--dashboard_s6b-002D-ixt-002D-zke.id}"
+}
+
+output "datadog_dashboard_tfer--dashboard_u82-002D-zxp-002D-ead_id" {
+  value = "${datadog_dashboard.tfer--dashboard_u82-002D-zxp-002D-ead.id}"
+}
